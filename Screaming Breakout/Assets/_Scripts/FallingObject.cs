4483ca3de984b979820205120b3de91777557633
johncollins0 <johnc1999@hotmail.com>
2020-07-07 19:09:32 +1000

    Added falling objects, they are static until hit by the ball at which point they begin to fall

---
+++ Screaming Breakout/Assets/_Scripts/FallingObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+
+    public bool fallOnHit = true;
+    public float gravityScale;
+    public float bounceForce;
+    private bool released = false;
+    private float releaseTime;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (released && Time.time >= releaseTime)
+        {
+            rb.gravityScale = gravityScale;
+            released = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 11)   // Falling object was hit by the ball
+        {
+            //rb.gravityScale = gravityScale;
+            releaseTime = Time.time + Time.deltaTime * 2;
+            released = true;
+        }
+        if (collision.gameObject.tag == "Hazzard")  // If the falling object hits the red blocker below the paddle. This might need to be changed later if we change how that works but is fine until then. 
+        {
+            Destroy(this.gameObject, 0.2f);
+        }
+        if (collision.gameObject.layer == 8)    // Falling object hit the paddle, make it bounce off and fly off screen
+        {
+            GameObject attractor = GameObject.Find("Out of Bounds Attractor");
+            Vector3 towardsAttractor = Vector3.Normalize(attractor.transform.position - transform.position);
+            if (transform.position.x > 0) {
+                towardsAttractor = new Vector3(-towardsAttractor.x, towardsAttractor.y, towardsAttractor.z);
+            }
+            rb.AddForce((towardsAttractor * bounceForce), ForceMode2D.Impulse);
+        }
+    }
+}
